e388e864c1a646c6391246087895ab3f7c753865
hamzah <alihamzah1001@gmail.com>
2025-04-19 14:27:46 -0400

    fixed firebase expenses

--- src/app/supervisorDash/page.tsx
+++ src/app/supervisorDash/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import { getFirestore, collection, getDocs, updateDoc, doc, query, where } from 'firebase/firestore';
 import { app } from '../firebase/firebaseConfig';
